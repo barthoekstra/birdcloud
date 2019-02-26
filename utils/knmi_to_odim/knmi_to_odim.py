5f1404e945ffe9618a98e882472a92b04ded4988
Bart Hoekstra <barthoekstra@gmail.com>
2019-02-26 09:02:56 +0100

    Make compatible with new folder structure and add error check

--- utils/knmi_to_odim/knmi_to_odim.py
+++ utils/knmi_to_odim/knmi_to_odim.py
@@ -1,12 +1,14 @@
 """
 description: Converts KNMI formatted HDF5 files to ODIM formatted HDF5 files
 author: Bart Hoekstra
+
+@TODO: Add logging so conversion errors can be stored
 """
 import glob
 import subprocess
 import time
 
-knmi_files = glob.glob('../data/raw/*.h5')
+knmi_files = glob.glob('../../data/raw/*.h5')
 
 start_time = time.time()
 
@@ -16,6 +18,10 @@ for knmi_file in knmi_files:
     odim_file = knmi_file.replace('.h5', '_ODIM.h5').replace('../data/raw/', '../data/interim/')
 
     cmd = './KNMI_vol_h5_to_ODIM_h5 {} {}'.format(odim_file, knmi_file)
-    subprocess.run(cmd, shell=True, check=True)
+    try:
+        subprocess.run(cmd, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(e)
+        continue
 
 print('Elapsed time: {}'.format(time.time() - start_time))
