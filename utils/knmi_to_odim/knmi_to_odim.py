a69758107ccea4f46f25d71f51d32a5521dc959a
Bart Hoekstra <barthoekstra@gmail.com>
2019-03-18 21:38:51 +0100

    Parallelised ODIM conversion

--- utils/knmi_to_odim/knmi_to_odim.py
+++ utils/knmi_to_odim/knmi_to_odim.py
@@ -4,24 +4,48 @@ author: Bart Hoekstra
 
 @TODO: Add logging so conversion errors can be stored
 """
-import glob
+import os
 import subprocess
 import time
+from pathlib import Path
+from multiprocessing import Pool, cpu_count, current_process
+import tarfile
 
-knmi_files = glob.glob('../../data/raw/*.h5')
+path_raw = Path('/') / 'Users' / 'barthoekstra' / 'Development' / 'birdcloud' / 'data' / 'raw' / '201708'
+print(path_raw)
+path_converted = path_raw.parent.parent / 'interim' / '201708_ODIM'
 
 start_time = time.time()
 
-for knmi_file in knmi_files:
-    print('Converting: {}'.format(knmi_file))
 
-    odim_file = knmi_file.replace('.h5', '_ODIM.h5').replace('../data/raw/', '../data/interim/')
+def unpack_file(file):
+    tar = tarfile.open(file)
+    tar.extractall(path=path_raw)
+    tar.close()
 
-    cmd = './KNMI_vol_h5_to_ODIM_h5 {} {}'.format(odim_file, knmi_file)
+
+with Pool(processes=6) as pool:
+    files = [file for file in path_raw.glob('**/*.tar')]
+    pool.map(unpack_file, files)
+
+
+# Apparently unpacking is finished
+
+def convert_knmi_file(file):
+    odim_file = path_converted / file.name.replace('.h5', '_ODIM.h5')
+
+    cmd = './KNMI_vol_h5_to_ODIM_h5 {} {}'.format(odim_file, file)
+
     try:
         subprocess.run(cmd, shell=True, check=True)
     except subprocess.CalledProcessError as e:
         print(e)
-        continue
+    else:
+        os.remove(file)
+
+with Pool(processes=6) as pool:
+    files = [file for file in path_raw.glob('*.h5')]
+    pool.map(convert_knmi_file, files)
+
 
 print('Elapsed time: {}'.format(time.time() - start_time))
